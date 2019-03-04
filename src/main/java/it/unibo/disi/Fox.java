6b2600169aafe896ff065ed7532da27c185cae2f
Luigi Asprino <luigi.asprino@gmail.com>
2019-03-04 17:05:52 +0100

    Alignment based methods

--- src/main/java/it/unibo/disi/Fox.java
+++ src/main/java/it/unibo/disi/Fox.java
@@ -1,4 +1,6 @@
-package it.unibo.disi ;
+package it.unibo.disi;
+
+import java.io.IOException;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
@@ -6,16 +8,27 @@ import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import it.unibo.disi.features.SENECA;
+import it.unibo.disi.features.Tipalo;
+
 public class Fox {
 
 	private static Logger logger = LoggerFactory.getLogger(Fox.class);
 
-	public static void main(String[] args){	
+	public static void main(String[] args) {
 		try {
 			Configurations configs = new Configurations();
-			Configuration config = configs.properties("config.properties");
-			logger.info(config.getString("prova"));
+			Configuration config = configs.properties(args[0]);
+
+			SENECA seneca = SENECA.getInstance(config.getString("SENECA_classes"), config.getString("SENECA_PhysicalObjects"));
+			Tipalo tipalo = Tipalo.getInstance(config.getString("Tipalo_classes"), config.getString("Tipalo_PhysicalObjects"));
+
+			logger.info("{}", seneca.isPhysicalObject("http://dbpedia.org/resource/Xanthophyll"));
+			logger.info("{}", tipalo.isPhysicalObject("http://dbpedia.org/resource/Xanthophyll"));
+
 		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
--- src/main/java/it/unibo/disi/utils/Utils.java
+++ src/main/java/it/unibo/disi/utils/Utils.java
@@ -1,5 +1,8 @@
 package it.unibo.disi.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +21,8 @@ public class Utils {
 	 * 
 	 * A very simple tokenizer based on PTBTokenizer of Stanford's CoreNLP library.
 	 * 
-	 * @param text The input text to tokenize. 
+	 * @param text
+	 *            The input text to tokenize.
 	 * @return A list of tokens contained in the input text.
 	 */
 	public static List<String> tokenize(String text) {
@@ -28,6 +32,24 @@ public class Utils {
 			CoreLabel label = ptbt.next();
 			result.add(label.originalText());
 		}
+		return result;
+	}
+
+	/**
+	 * Read file passed as parameter and return a list of strings (one per line without line terminator '\n').
+	 *
+	 * @param filePath the file to read
+	 * @return a list of strings 
+	 * @throws IOException
+	 */
+	public static List<String> readFileToListString(String filePath) throws IOException {
+		List<String> result = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(filePath));
+		String line;
+		while ((line = br.readLine()) != null) {
+			result.add(line);
+		}
+		br.close();
 		return result;
 	}
 
