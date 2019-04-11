a231d01482fd5630883b0e44760b4a8f66550a2f
Luigi Asprino <luigi.asprino@gmail.com>
2019-04-11 19:17:58 +0200

    Comments added

--- src/main/java/it/unibo/disi/experiments/WekaExperiments.java
+++ src/main/java/it/unibo/disi/experiments/WekaExperiments.java
@@ -14,21 +14,25 @@ public class WekaExperiments {
 	public static void main(String[] args) {
 
 		try {
+			
+			// Load instances 
+			// Note: pass the filepath of the XRFF as main parameter
 			Instances instances = WekaUtils.loadXRFFInstances(args[0]);
+			
+			// Create a new classifier
 			SMO svm = new SMO();
 
+			// remove the attribute id from instances
 			Attribute idAtt = instances.attribute("ID");
-
 			FilteredClassifier fc = new FilteredClassifier();
 			fc.setClassifier(svm);
 			Remove rm = new Remove();
 			rm.setAttributeIndicesArray(new int[] { idAtt.index() });
 			fc.setFilter(rm);
 
+			// 10 fold cross-validation of SVM on dataset input dataset
 			Evaluation eval = new Evaluation(instances);
-
 			eval.crossValidateModel(fc, instances, 10, new Random(System.currentTimeMillis()));
-
 			System.out.println(eval.toSummaryString("\nResults\n\n", false));
 			double[][] cm = eval.confusionMatrix();
 			System.out.println(String.format("%d\t%d\n%d\t%d\n", (int) cm[0][0], (int) cm[0][1], (int) cm[1][0], (int) cm[1][1]));
