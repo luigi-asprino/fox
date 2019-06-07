2a1466c5af7476605a0c3ce105f3b77ccf584c9b
Luigi Asprino <luigi.asprino@gmail.com>
2019-06-07 09:13:51 +0200

    Unused Import

--- src/main/java/it/unibo/disi/fox/model/MulticlassEntity.java
+++ src/main/java/it/unibo/disi/fox/model/MulticlassEntity.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import it.unibo.disi.fox.utils.Utils;
-
 public class MulticlassEntity {
 
 	private List<ClassBelonging> classBelonging = new ArrayList<>();
